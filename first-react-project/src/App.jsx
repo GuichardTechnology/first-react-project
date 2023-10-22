91baea5bac87001de8a9562693bcd1cbaf7b13b8
GuichardTechnology <guichardj483@gmail.com>
2023-10-22 16:50:12 -0300

    first-react-project

---
+++ first-react-project/src/App.jsx
@@ -0,0 +1,23 @@
+ export default function App(){
+  return(
+    <div className="app">
+      <img src="/react-img.png" alt="logo"/>
+      <h1>I learn react</h1>
+      <p>React is amazing</p>
+      <div>
+        <button>My button</button>
+        <button>I love react</button>
+        <hr/>
+        <h2>Do you like react</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing 
+          elit. Quasi vero eligendi iste eveniet iure! Nihil 
+           quos perferendis libero? Dignissimos aspernatur quis corrupti,
+           accusantium culpa pariatur illo recusandae debitis dolore.</p>
+      </div>
+    </div>
+
+    
+  )
+
+
+}
